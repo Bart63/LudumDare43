79c39797532203b31ef3886a48b05f98c7cb49f4
zeref80 <12345qaz6789@wp.pl>
2018-12-01 16:39:10 +0100

    Health

---
+++ Assets/HealthWeight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthWeight : MonoBehaviour {
+    public int MaxHealth;
+    public int CurrentHealth;
+    HeightOfBaloon script;
+
+    private void Start()
+    {
+        script = FindObjectOfType<HeightOfBaloon>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && script.currentFood>0)
+        {
+            CurrentHealth += 1;
+            script.currentFood -= 1;
+        }
+    }
+
+ private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject tr = new GameObject(), hit = collision.gameObject;
+
+        if (hit.layer == 13)
+        {
+            Destroy(hit);
+            CurrentHealth -= 3;
+        }
+    }
+}
