5a0a64ec48710457391ceba5ca07519ac5972697
Bart <bartek.durys@gmail.com>
2018-12-01 15:22:03 +0100

    Throw a ball

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour
+{
+    public Transform LeftBottom;
+    public GameObject bullet;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 velo = LeftBottom.position + Input.mousePosition - transform.GetChild(0).position;
+            GameObject b = Instantiate(bullet, new Vector3(0,0,0), Quaternion.identity, transform);
+            b.transform.localPosition = new Vector3(0, 0, 0);
+            b.GetComponent<Rigidbody2D>().AddForce(velo*20);
+        }
+
+    }
+}
