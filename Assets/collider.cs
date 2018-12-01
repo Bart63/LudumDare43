ee5ea2e9ae520a9ea2097009d21b9ee16896fd8e
zeref80 <12345qaz6789@wp.pl>
2018-12-01 16:00:38 +0100

    collider

---
+++ Assets/collider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collider : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject tr = new GameObject(), hit = collision.gameObject;
+
+        if (hit.layer == 13)
+        {
+            Destroy(hit);
+            Destroy(gameObject);
+        }
+    }
+}
