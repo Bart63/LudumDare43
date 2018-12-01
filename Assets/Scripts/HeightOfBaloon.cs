e9c452787e131262c7a623a5b4ab7a478df8432b
Bart <bartek.durys@gmail.com>
2018-12-01 11:32:28 +0100

    Having fun with containers

--- Assets/Scripts/HeightOfBaloon.cs
+++ Assets/Scripts/HeightOfBaloon.cs
@@ -7,8 +7,15 @@ public class HeightOfBaloon : MonoBehaviour {
     public Transform maxHeight, minHeight;
     public int maxFood, currentFood;
     private float ymax, ymin;
-    void Start () {
-	}
+    void Start ()
+    {
+        ymax = maxHeight.position.y;
+        ymin = minHeight.position.y;
+
+        float currentDestination = (currentFood * ymin + (maxFood - currentFood) * ymax) / maxFood;
+
+        transform.position = new Vector3(transform.position.x, currentDestination, transform.position.z);
+    }
 	
 
 	void Update () {
---
+++ Assets/Scripts/MakeChildrenGoLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeChildrenGoLeft : MonoBehaviour {
+
+    public float speed = 1f;
+
+    List<RectTransform> childrenTransform = new List<RectTransform>();
+
+	void Start () {
+		for(int i=0; i<transform.childCount; i++)
+        {
+            childrenTransform.Add(transform.GetChild(i).GetComponent<RectTransform>());
+        }
+	}
+	
+	void Update () {
+        float delta = Time.deltaTime*speed;
+
+        foreach (RectTransform rt in childrenTransform)
+        {
+            rt.position = new Vector3(rt.position.x-delta, rt.position.y, rt.position.z);
+        }
+	}
+}
