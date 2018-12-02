50b42571a468d609b798e13bf886348e5cdc3ede
zeref80 <12345qaz6789@wp.pl>
2018-12-02 14:40:46 +0100

    poprawki Main Manu

---
+++ Assets/Volume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    public Slider VolumeSlider;
+    public AudioSource VolumeAudio;
+
+    public void VolumeController()
+    {
+        VolumeAudio.volume = VolumeSlider.value;
+    }
+}
